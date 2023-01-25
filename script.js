a7bf64097182879aac9989e47b9df637a418c95b
sajjad ghorbani <sajjadr2001@gmail.com>
2023-01-25 11:34:04 +0330

    Code Chalenge #3

--- script.js
+++ script.js
@@ -33,3 +33,44 @@ if(markHigherBMI)
     console.log(`Mark's BMI ${(MarkBMI)} is higher than John's ${(JohnBMI)}!`)
 else
     console.log(`John's BMI ${(JohnBMI)} is higher than Mark's ${(MarkBMI)}!`) */
+    
+//-----------------------------
+
+//Code Challenge #3
+/* Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy */
+
+/*
+let dolphinAvgScore = 0; 
+let koalasAvgScore = 0; 
+
+for(let i = 0; i < 3; i++){
+    dolphinAvgScore += +prompt(`enter dolphin score ${i+1}:`,0);
+    koalasAvgScore += +prompt(`enter koalas score ${i+1}:`,0);
+
+}
+
+dolphinAvgScore /= 3;
+koalasAvgScore /= 3;
+
+if(dolphinAvgScore > koalasAvgScore && dolphinAvgScore >= 100)
+    console.log("Dolphins win!")
+else if(dolphinAvgScore < koalasAvgScore && koalasAvgScore >= 100)
+    console.log("koalas win!");
+else if(dolphinAvgScore == koalasAvgScore && dolphinAvgScore >= 100 && koalasAvgScore >= 100)
+    console.log("Draw.");
+else
+    console.log("no team wins the trophy");
+*/
+
+//------------------------------
