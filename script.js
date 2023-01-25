9ea800618a5d21132f0f10f1a00d1d109706b937
sajjad ghorbani <sajjadr2001@gmail.com>
2023-01-25 11:04:58 +0330

    Code Challenge #2

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ alert(markHigherBMI); */
 
 //-----------------------------
 
+//Code Challenge #2
+//Use a template literal to include the BMI values in the outputs. Example: "Mark's
+//BMI (28.3) is higher than John's (23.9)!"
+
+/*
+let MarkMass = prompt("enter marks mass",1);
+let MarkHieght = prompt("enter marks Hieght",1);
+let JohnMass = prompt("enter John mass",1);
+let JohnHieght = prompt("enter John Hieght",1);
+
+let MarkBMI = MarkMass / (MarkHieght **2);
+let JohnBMI = JohnMass / (JohnHieght **2);
+
+let markHigherBMI = MarkBMI > JohnBMI;
+
+if(markHigherBMI)
+    console.log(`Mark's BMI ${(MarkBMI)} is higher than John's ${(JohnBMI)}!`)
+else
+    console.log(`John's BMI ${(JohnBMI)} is higher than Mark's ${(MarkBMI)}!`) */
