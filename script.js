9a5d20eed87d639d0fc02e5cc37aa3e0f64506e6
sajjad ghorbani <sajjadr2001@gmail.com>
2023-01-25 10:41:52 +0330

    Code Challenge #1

---
+++ script.js
@@ -0,0 +1,16 @@
+//Code Challenge #1
+//Mark and John are trying to compare their BMI (Body Mass Index)
+/*
+let MarkMass = prompt("enter marks mass",1);
+let MarkHieght = prompt("enter marks Hieght",1);
+let JohnMass = prompt("enter John mass",1);
+let JohnHieght = prompt("enter John Hieght",1);
+
+let MarkBMI = MarkMass / (MarkHieght **2);
+let JohnBMI = JohnMass / (JohnHieght **2);
+
+let markHigherBMI = MarkBMI > JohnBMI;
+alert(markHigherBMI); */
+
+//-----------------------------
+
